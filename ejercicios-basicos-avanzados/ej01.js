ec55161e4337f0f0f5cc4fcff3dc1b65013e8551
fishellVvv <victorvigilvela@gmail.com>
2025-10-05 23:17:13 +0200

    ejercicio 01

---
+++ ejercicios-basicos-avanzados/ej01.js
@@ -0,0 +1,17 @@
+// 1.
+let miSuperheroeFavorito = "Hulk";
+
+// 2.
+let numeroFavorito = 50;
+
+// 3.
+let altura = 5;
+let longitud = 10;
+
+// 4.
+let suma = altura + longitud;
+
+// 5.
+console.log("Mi superheroe favorito es " + miSuperheroeFavorito);
+console.log("Mi número favorito es el " + numeroFavorito);
+console.log("Si la altura es de " + altura + "m y la longitud de " + longitud + "m, la suma de ambas es de " + suma + "m");
