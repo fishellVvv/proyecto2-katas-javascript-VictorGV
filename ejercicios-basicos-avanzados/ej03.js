e73835aa34aa98ff2cdabf1677aac1e8af9267d0
fishellVvv <victorvigilvela@gmail.com>
2025-10-05 23:57:29 +0200

    ejercicio 03

---
+++ ejercicios-basicos-avanzados/ej03.js
@@ -0,0 +1,20 @@
+// 3.1
+console.log(10 * 5);
+
+// 3.2
+console.log(10 / 2);
+
+// 3.3
+console.log(15 % 9);
+
+// 3.4
+let p = 10, j = 5;
+let o = p;
+o += j;
+console.log(o);
+
+// 3.5
+let c = 10, m = 5;
+let i = c;
+i *= m;
+console.log(i);
