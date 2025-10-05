cf181430df7d1aabfb53cc55e9f3a97fe1f9652e
fishellVvv <victorvigilvela@gmail.com>
2025-10-06 00:13:48 +0200

    ejercicio 04

---
+++ ejercicios-basicos-avanzados/ej04.js
@@ -0,0 +1,19 @@
+const aldeanos = ["Fibrilio", "Narciso", "Vacarena", "Tendo", "Nendo"];
+
+// 4.1
+console.log(aldeanos[3]);
+
+// 4.2
+aldeanos.push("Cervasio");
+
+// 4.3
+aldeanos[0] = "Bambina";
+
+// 4.4
+aldeanos.reverse();
+
+// 4.5
+aldeanos.splice(aldeanos.indexOf("Narciso"), 1, "Canela");
+
+// 4.6
+console.log(aldeanos[aldeanos.length - 1]);
