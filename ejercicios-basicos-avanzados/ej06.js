2604a3fd4733a7c5851b1bf7ada66d00bc4cad1e
fishellVvv <victorvigilvela@gmail.com>
2025-10-06 13:19:31 +0200

    ejercicio 06

---
+++ ejercicios-basicos-avanzados/ej06.js
@@ -0,0 +1,20 @@
+// 6.1
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// 6.2
+for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// 6.3
+for (let i = 0; i < 10; i++) {
+    if (i == 9) {
+        console.log("¡Dormido!");
+    } else {
+        console.log("Intentando dormir 🐑");
+    }
+}
