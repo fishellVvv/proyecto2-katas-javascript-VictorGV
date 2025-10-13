45730a4636ee0be3cbbd9eb3f7012e3894030aa7
fishellVvv <victorvigilvela@gmail.com>
2025-10-13 04:50:41 +0200

    ejercicio 14

---
+++ ejercicios-basicos-avanzados/ej14.js
@@ -0,0 +1,26 @@
+const words = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code'
+];
+
+const repeatCounter = (list) => {
+  const countedWords = {};
+  for (const w of list) {
+    if (!(w in countedWords)) {
+      countedWords[w] = 0;
+    }
+    countedWords[w]++;
+  }
+  return countedWords;
+}
+
+console.log(repeatCounter(words))
