72051a7dd4b98fdf432053d8f4d76ce69a302632
fishellVvv <victorvigilvela@gmail.com>
2025-10-13 15:07:04 +0200

    ejercicio 16

---
+++ ejercicios-basicos-avanzados/ej16.js
@@ -0,0 +1,12 @@
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+for (const item of placesToTravel) {
+    console.log(item);
+};
