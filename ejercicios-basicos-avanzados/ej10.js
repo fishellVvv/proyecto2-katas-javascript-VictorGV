1780d8d2b45df830a9a853225561eaf713be8dd5
fishellVvv <victorvigilvela@gmail.com>
2025-10-13 02:07:40 +0200

    ejercicio 10

---
+++ ejercicios-basicos-avanzados/ej10.js
@@ -0,0 +1,9 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+
+function average(numberList) {
+  let sum = 0;
+  for (const number of numberList) {
+    sum += number;
+  }
+  return sum / numbers.length;
+}
