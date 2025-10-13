efe4e1d59cfa8fe186eebe9b7ee41c7d4c5d6482
fishellVvv <victorvigilvela@gmail.com>
2025-10-13 02:19:39 +0200

    ejercicio 11

---
+++ ejercicios-basicos-avanzados/ej11.js
@@ -0,0 +1,23 @@
+const mixedElements = [
+  6,
+  1,
+  "Marvel",
+  1,
+  "hamburguesa",
+  "10",
+  "Prometeo",
+  8,
+  "Hola mundo",
+];
+
+function averageWord(list) {
+  let sum = 0
+  for (const item of list) {
+    if (typeof(item) == "number") {
+        sum += item;
+    } else {
+        sum += item.length;
+    }
+  }
+  return sum
+}
