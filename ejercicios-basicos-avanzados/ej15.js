51203e510834d4fcda16c97e6eadf5c62cb10cd6
fishellVvv <victorvigilvela@gmail.com>
2025-10-13 14:51:31 +0200

    ejercicio 15

---
+++ ejercicios-basicos-avanzados/ej15.js
@@ -0,0 +1,14 @@
+const products = [
+  "Camiseta de Metallica",
+  "Pantalón vaquero",
+  "Gorra de beisbol",
+  "Camiseta de Basket",
+  "Cinturón de Orión",
+  "AC/DC Camiseta",
+];
+
+for (const item of products) {
+  if (item.includes("Camiseta")) {
+    console.log(item);
+  }
+}
