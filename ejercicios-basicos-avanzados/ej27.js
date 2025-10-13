6666fa823594f368b4e8de4ed08771da49fafaa5
fishellVvv <victorvigilvela@gmail.com>
2025-10-13 22:08:14 +0200

    ejercicio 27

---
+++ ejercicios-basicos-avanzados/ej27.js
@@ -0,0 +1,22 @@
+const cartoons = [
+  { name: 'Bugs Bunny', debut: 1938 },
+  { name: 'SpongeBob SquarePants', debut: 1999 },
+  { name: 'Tom and Jerry', debut: 1940 },
+  { name: 'Mickey Mouse', debut: 1928 },
+  { name: 'Scooby-Doo', debut: 1969 },
+  { name: 'The Flintstones', debut: 1960 },
+  { name: 'Batman: The Animated Series', debut: 1992 },
+  { name: 'The Simpsons', debut: 1989 },
+  { name: 'Pokémon', debut: 1997 },
+  { name: "Dexter's Laboratory", debut: 1996 }
+];
+
+const oldest = { name: "", debut: 9999 };
+for (const item of cartoons) {
+  if (item.debut < oldest.debut) {
+    oldest.name = item.name;
+    oldest.debut = item.debut;
+  };
+};
+
+console.log(oldest.name);
