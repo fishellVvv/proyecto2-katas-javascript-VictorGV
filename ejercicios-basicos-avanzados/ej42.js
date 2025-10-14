82fd7a7869d218d9dea7edde08d985e410843d31
fishellVvv <victorvigilvela@gmail.com>
2025-10-14 23:50:17 +0200

    ejercicio 42

---
+++ ejercicios-basicos-avanzados/ej42.js
@@ -0,0 +1,20 @@
+const fantasticFour = [
+  "La antorcha humana",
+  "Mr. Fantástico",
+  "La mujer invisible",
+  "La cosa",
+];
+
+const swap = (array, indexA, indexB) => {
+  if (indexA >= array.length || indexB >= array.length) return "Error: index out of bounds";
+
+  const swapedArray = array.slice();
+
+  swapedArray[indexA] = array[indexB];
+  swapedArray[indexB] = array[indexA];
+
+  return swapedArray;
+};
+
+console.log(swap(fantasticFour, 0, 1));
+console.log(swap(fantasticFour, 6, 4));
