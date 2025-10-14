d740f600d4a63e6601e4fcd8576b17e97fc5b4c4
fishellVvv <victorvigilvela@gmail.com>
2025-10-14 13:34:55 +0200

    ejercicio 35

---
+++ ejercicios-basicos-avanzados/ej35.js
@@ -0,0 +1,22 @@
+const mutants = [
+  { name: 'Wolverine', power: 'regeneration' },
+  { name: 'Magneto', power: 'magnetism' },
+  { name: 'Professor X', power: 'telepathy' },
+  { name: 'Jean Grey', power: 'telekinesis' },
+  { name: 'Rogue', power: 'power absorption' },
+  { name: 'Storm', power: 'weather manipulation' },
+  { name: 'Mystique', power: 'shape-shifting' },
+  { name: 'Beast', power: 'superhuman strength' },
+  { name: 'Colossus', power: 'steel skin' },
+  { name: 'Nightcrawler', power: 'teleportation' },
+];
+
+function findMutantByPower(mutants, power) {
+  let count = 0;
+  for (const mutant of mutants) {
+    if (mutant.power === power) count++;
+  };
+  return (count > 0) ? `${power} was found in ${count} mutant(s)` : `${power} was not found among mutants`;
+};
+
+console.log(findMutantByPower(mutants, "regeneration"));
