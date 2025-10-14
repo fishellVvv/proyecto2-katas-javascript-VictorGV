6d0eaef576bee904f2b37cecb58e6cb9fdfdad73
fishellVvv <victorvigilvela@gmail.com>
2025-10-14 13:58:50 +0200

    ejercicio 36

---
+++ ejercicios-basicos-avanzados/ej36.js
@@ -0,0 +1,25 @@
+const actors = [
+  { name: 'Leonardo DiCaprio', born: 1974 },
+  { name: 'Tom Hanks', born: 1956 },
+  { name: 'Meryl Streep', born: 1949 },
+  { name: 'Brad Pitt', born: 1963 },
+  { name: 'Johnny Depp', born: 1963 },
+  { name: 'Scarlett Johansson', born: 1984 },
+  { name: 'Jennifer Lawrence', born: 1990 },
+  { name: 'Denzel Washington', born: 1954 },
+  { name: 'Morgan Freeman', born: 1937 },
+  { name: 'Cate Blanchett', born: 1969 }
+];
+
+function calculateActorsAges(actors) {
+  const todayYear = new Date().getFullYear();
+  const actorAges = [];
+
+  for (const actor of actors) {
+  actorAges.push({ name: actor.name, age: todayYear - actor.born });
+  };
+
+  return actorAges;
+};
+
+console.log(calculateActorsAges(actors));
