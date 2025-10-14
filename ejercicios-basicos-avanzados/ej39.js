31ee260fbc375c13c4370f08e7f4f9471e3fcdcb
fishellVvv <victorvigilvela@gmail.com>
2025-10-14 22:54:09 +0200

    ejercicio 39

---
+++ ejercicios-basicos-avanzados/ej39.js
@@ -0,0 +1,52 @@
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+const countFavorites = (userList) => {
+  const soundsCounter = {};
+
+  for (const user of userList) {
+    for (const key in user.favoritesSounds) {
+      if (key in soundsCounter) {
+        soundsCounter[key]++;
+      } else {
+        soundsCounter[key] = 1;
+      };
+    };
+  };
+
+  return soundsCounter;
+};
+
+console.log(countFavorites(users));
