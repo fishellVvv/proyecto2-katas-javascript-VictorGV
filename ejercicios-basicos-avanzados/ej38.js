f6b27bf7a3eda8659ab9bed41fa0a849a7d9a833
fishellVvv <victorvigilvela@gmail.com>
2025-10-14 15:22:44 +0200

    ejercicio 38

---
+++ ejercicios-basicos-avanzados/ej38.js
@@ -0,0 +1,50 @@
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+const averageVolume = (userList) => {
+  let sum = 0;
+  let count = 0;
+  
+  for (const user of userList) {
+    for (const sound in user.favoritesSounds) {
+      sum += user.favoritesSounds[sound].volume;
+      count++;
+    };
+  };
+
+  return (sum / count);
+};
+
+console.log(averageVolume(users));
