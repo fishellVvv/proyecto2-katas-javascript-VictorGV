b97e5a82b359cd1d431b25df7b67832409060091
fishellVvv <victorvigilvela@gmail.com>
2025-10-22 21:51:54 +0200

    ES6+ ej08

---
+++ ejercicios-es6-asincronia/ej08/index.js
@@ -0,0 +1,39 @@
+let data = [];
+
+const getCharacters = async () => {
+  try {
+    const res = await fetch(`https://thronesapi.com/api/v2/Characters`);
+    data = await res.json();
+  } catch (er) {
+    console.error(er);
+  }
+};
+
+const printList = () => {
+  const characterList = document.querySelector("#character-list");
+
+  for (const character of data) {
+    const option = document.createElement("option");
+    option.textContent = character.fullName;
+    option.value = String(character.id);
+    characterList.appendChild(option);
+  }
+
+  const img = document.querySelector(".character-image");
+  img.src = data[0].imageUrl;
+  img.alt = data[0].fullName;
+
+  characterList.addEventListener("change", async (ev) => {
+    const id = Number(ev.target.value);
+    const res = await fetch(`https://thronesapi.com/api/v2/Characters/${id}`);
+    const character = await res.json();
+
+    img.src = character.imageUrl;
+    img.alt = character.fullName;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await getCharacters();
+  printList();
+});
