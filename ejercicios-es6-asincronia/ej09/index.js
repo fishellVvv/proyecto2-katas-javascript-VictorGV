9207fde429359c4afd0cae536117707bdc911137
fishellVvv <victorvigilvela@gmail.com>
2025-10-22 22:21:50 +0200

    ES6+ ej09

---
+++ ejercicios-es6-asincronia/ej09/index.js
@@ -0,0 +1,21 @@
+const img = document.querySelector(".random-image");
+const randomNumber = Math.floor(Math.random() * 152);
+
+const getPokemon = async (id) => {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    pokemon = await res.json();
+  } catch (er) {
+    console.error(er);
+  }
+};
+
+const printPokemon = (pokemon) => {
+  img.src = pokemon.sprites.other.dream_world.front_default;
+  img.alt = pokemon.name;
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await getPokemon(randomNumber);
+  printPokemon(pokemon);
+});
