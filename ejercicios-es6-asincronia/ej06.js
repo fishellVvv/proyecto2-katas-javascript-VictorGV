e1cc0dfcbb51465891adfe873a92c925d71b1eb9
fishellVvv <victorvigilvela@gmail.com>
2025-10-22 19:07:18 +0200

    ES6+ ej06

---
+++ ejercicios-es6-asincronia/ej06.js
@@ -0,0 +1,28 @@
+// 6.1
+const exams = [
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
+];
+
+const totalScore = exams.reduce((acc, exam) => acc + exam.score, 0);
+console.log("6.1 ->", totalScore);
+
+// 6.2
+const totalPassedScore = exams.reduce(
+  (acc, exam) => (exam.score >= 5 ? acc + exam.score : acc),
+  0
+);
+console.log("6.2 ->", totalPassedScore);
+
+// 6.3
+const averageScore =
+  exams.reduce((acc, exam) => acc + exam.score, 0) / exams.length;
+console.log("6.3 ->", averageScore);
